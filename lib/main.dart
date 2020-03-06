d2da5475f44f833389bae678c780db3aee6c7075
officicalchriseo <officialchriseo@gmail.com>
2020-03-06 21:11:01 +0100

    adding stateless widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,41 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Scaffold(
-    appBar: AppBar(
-      title: Text("New App Bar",
-      style: TextStyle(
-        fontSize: 20.0,
-        fontWeight: FontWeight.bold,
-        color: Colors.white,
-      ),),
-      centerTitle: true,
-      backgroundColor: Colors.blue[600],
-    ),
-    body: Center(
-      child: Text('Flutter Ninja',
-      style: TextStyle(
-        fontWeight: FontWeight.bold,
-        fontStyle: FontStyle.italic,
-        color: Colors.blueAccent,
-        fontSize: 20.0,
-        fontFamily: 'IndieFlower',
-      ),
-      ),
-    ),
-    floatingActionButton: FloatingActionButton(
-      onPressed: () {},
-      child: Text('Click'),
-    ),
-  ),
+  home: Home()
 ));
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("New App Bar",
+          style: TextStyle(
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),),
+        centerTitle: true,
+        backgroundColor: Colors.blue[600],
+      ),
+      body: Center(
+        child: Text('Flutter Ninja',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontStyle: FontStyle.italic,
+            color: Colors.blueAccent,
+            fontSize: 20.0,
+            fontFamily: 'IndieFlower',
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Text('Click'),
+      ),
+    );
+  }
+}
 
 
 
