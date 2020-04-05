7c9a4fca4c9824d8e88cc21f9c795c407b6f0ae3
officicalchriseo <officialchriseo@gmail.com>
2020-04-05 18:26:44 +0100

    stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,37 +1,112 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-  home: Home()
-));
+void main() => runApp(MaterialApp(home: Home()));
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int ninjaLevel = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[600],
       appBar: AppBar(
         title: Text(
-          'Hello Ninja',
-          style: TextStyle(
-              fontSize: 20.0,
-              fontWeight: FontWeight.bold,
-              color: Colors.white,
-              fontFamily: 'IndieFlower'
-          ),
+          'Hello Flutter',
         ),
-        backgroundColor: Colors.red[600],
         centerTitle: true,
+        elevation: 0.0,
       ),
-      body: Center(
-        child: Image.asset('assets/akeju.jpg'),
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage(''),
+                radius: 40.0,
+              ),
+            ),
+            Divider(
+              height: 60.0,
+              color: Colors.grey,
+            ),
+            Text(
+              'Name',
+              style: TextStyle(
+                fontSize: 20.0,
+                letterSpacing: 2.0,
+                color: Colors.grey,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 8.0),
+            Text(
+              'First Flutter UI',
+              style: TextStyle(
+                fontSize: 30.0,
+                color: Colors.amberAccent,
+                letterSpacing: 2.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 20.0),
+            Text(
+              'UI Level',
+              style: TextStyle(
+                fontSize: 20.0,
+                letterSpacing: 2.0,
+                color: Colors.grey,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 10.0),
+            Text(
+              '$ninjaLevel',
+              style: TextStyle(
+                fontSize: 30.0,
+                color: Colors.amberAccent,
+                letterSpacing: 2.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 20.0),
+            Row(
+              children: <Widget>[
+                Icon(
+                  Icons.mail,
+                  color: Colors.white,
+                  size: 30.0,
+                ),
+                SizedBox(width: 10.0,),
+                Text(
+                  'ekenechristian62@gmail.com',
+                  style: TextStyle(
+                    fontSize: 15.0,
+                    letterSpacing: 2.0,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: Text('Click'),
-        backgroundColor: Colors.red[600],
+        onPressed: () {
+          setState(() {
+            ninjaLevel+=1 ;
+          });
+        },
+        child: Icon(
+          Icons.add,
+          color: Colors.white,
+        ),
       ),
     );
   }
 }
-
-
-
